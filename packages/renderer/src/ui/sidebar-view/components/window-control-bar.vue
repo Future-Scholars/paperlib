26ae02789a32c09e4040040bde0d45255dc8dbcd
geoffreychen777 <geoffreychen777@gmail.com>
2022-06-05 15:56:49 +0100

    + fix windows window control bar click bug

--- packages/renderer/src/ui/sidebar-view/components/window-control-bar.vue
+++ packages/renderer/src/ui/sidebar-view/components/window-control-bar.vue
@@ -16,17 +16,20 @@ const onMaximizeClicked = () => {
 
 <template>
   <div class="flex w-full h-12 p-5 space-x-2 draggable-title">
-    <div class="group w-3 h-3 rounded-md bg-red-400" @click="onCloseClicked">
+    <div
+      class="group w-3 h-3 rounded-md bg-red-400 nodraggable-item"
+      @click="onCloseClicked"
+    >
       <BIconX class="group-hover:visible invisible w-3 h-3 text-gray-700" />
     </div>
     <div
-      class="group w-3 h-3 rounded-md bg-yellow-500"
+      class="group w-3 h-3 rounded-md bg-yellow-500 nodraggable-item"
       @click="onMinimizeClicked"
     >
       <BIconDash class="group-hover:visible invisible w-3 h-3 text-gray-700" />
     </div>
     <div
-      class="group w-3 h-3 rounded-md bg-green-500"
+      class="group w-3 h-3 rounded-md bg-green-500 nodraggable-item"
       @click="onMaximizeClicked"
     >
       <BIconPlus class="group-hover:visible invisible w-3 h-3 text-gray-700" />
