833c33663c6f47ddec49456c9eb27b19e66bf77d
geoffreychen777 <geoffreychen777@gmail.com>
2022-06-19 17:49:34 +0100

    don't show what's new

--- packages/preload/interactors/app-interactor.ts
+++ packages/preload/interactors/app-interactor.ts
@@ -170,7 +170,8 @@ export class AppInteractor {
   // ============================================================
   async shouldShowWhatsNew() {
     const lastVersion = this.getPreference("lastVersion");
-    return lastVersion !== (await this.version());
+    // return lastVersion !== (await this.version());
+    return false;
   }
 
   async hideWhatsNew() {
