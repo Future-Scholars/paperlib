c377a8a43d2afeee1e1c1dc2a3a953609cf7d518
GeoffreyChen777 <geoffreychen777@gmail.com>
2023-05-25 14:46:48 +0100

    Create word-comm-cert.ts

---
+++ app/utils/crypto/word-comm-cert.ts
@@ -0,0 +1,16 @@
+export const certs = {
+  key: `-----BEGIN PRIVATE KEY-----
+test
+-----END PRIVATE KEY-----`,
+  cert: `-----BEGIN CERTIFICATE-----
+test
+-----END CERTIFICATE-----
+-----BEGIN CERTIFICATE-----
+test
+-----END CERTIFICATE-----
+-----BEGIN CERTIFICATE-----
+test
+-----END CERTIFICATE-----
+
+`,
+};
