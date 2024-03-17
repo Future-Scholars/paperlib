f559238496d0562801e354c44dd70e07ac8bbb0f
igoogolx <27353191+igoogolx@users.noreply.github.com>
2024-03-17 20:16:14 +0800

    feat(sidebar): app logo

--- app/renderer/ui/sidebar-view/sidebar-view.vue
+++ app/renderer/ui/sidebar-view/sidebar-view.vue
@@ -4,6 +4,9 @@ import SwitcherTitle from "./components/switcher-title.vue";
 import WindowControlBar from "./components/window-control-bar.vue";
 import SidebarFeedsView from "./sidebar-feeds-view.vue";
 import SidebarLibraryView from "./sidebar-library-view.vue";
+import { onMounted, ref, watch } from "vue";
+import darkLogo from "@/renderer/assets/logo-dark.png";
+import lightLogo from "@/renderer/assets/logo-light.png";
 
 const uiState = uiStateService.useState();
 
@@ -17,16 +20,27 @@ const onViewContentSwitch = (view: number) => {
   }
   uiState.contentType = ["library", "feed"][view];
 };
+
+const darkMode = ref(false);
+onMounted(async () => {
+  darkMode.value = await PLMainAPI.windowProcessManagementService.isDarkMode();
+});
+preferenceService.on("preferedTheme", async () => {
+  darkMode.value = await PLMainAPI.windowProcessManagementService.isDarkMode();
+});
 </script>
 
 <template>
   <div class="flex-none flex flex-col w-full h-screen justify-between">
     <WindowControlBar class="flex-none" v-if="uiState.os !== 'win32'" />
-    <div class="h-6 draggable-title relative" v-if="uiState.os === 'win32'">
-      <img
-        class="w-6 h-6 absolute left-5 top-1"
-        src="@/renderer/assets/icon.png"
-      />
+    <div
+      class="h-8 draggable-title flex w-full pl-6"
+      v-if="uiState.os === 'win32'"
+    >
+      <div class="flex-row flex items-center">
+        <img class="w-4 h-4" :src="darkMode ? darkLogo : lightLogo" />
+        <span class="text-xs pl-2">Paperlib</span>
+      </div>
     </div>
 
     <SwitcherTitle
