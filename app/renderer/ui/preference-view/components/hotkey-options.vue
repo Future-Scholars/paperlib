c3a4c7089880d319d0127fe0bec0cb28985a5709
igoogolx <27353191+igoogolx@users.noreply.github.com>
2024-03-22 23:58:38 +0800

    feat(hotkeys): sort

--- app/renderer/ui/preference-view/components/hotkey-options.vue
+++ app/renderer/ui/preference-view/components/hotkey-options.vue
@@ -1,5 +1,5 @@
 <script setup lang="ts">
-import { computed, ref, watch } from "vue";
+import { ref, watch } from "vue";
 import { BIconX } from "bootstrap-icons-vue";
 
 const PRIMARY_KEYS = ["Control", "Command"];
